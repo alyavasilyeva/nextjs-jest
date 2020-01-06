176c6c576bdde4bd16d567e1c31a1c9c6aba5a12
alyavasilyeva <delirium.of.insane@gmail.com>
2020-01-06 10:14:08 +0300

    Example with mock-files

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import Image from './image.png';
 
 export default function Header() {
-  return <h1 className='title'>Welcome to Next.js!</h1>;
+  return (
+    <div>
+      <img src={Image} />
+      <h1 className='title'>Welcome to Next.js!</h1>
+    </div>
+  );
 }
--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   setupFiles: ['<rootDir>/jest.setup.js'],
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/']
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.js'
+  }
 };
